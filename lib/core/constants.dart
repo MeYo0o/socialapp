f3e92bb1096d9a87fa5c3c6489fa3b1df6070574
Moaz <67188321+MeYo0o@users.noreply.github.com>
2022-05-06 15:05:20 +0200

    Add Constants - Backend

---
+++ lib/core/constants.dart
@@ -0,0 +1,5 @@
+abstract class AppSettings {
+  static const String appEndPoint = 'http://meyo.ddns.net:3333/v1';
+  static const String projectId = '62721b7fcb0281e68ff1';
+  static const bool appSelfSigned = true;
+}
