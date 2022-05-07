348327bb07f27ede73e90be9c57f0e519e91aefe
Moaz <67188321+MeYo0o@users.noreply.github.com>
2022-05-07 06:04:34 +0200

    Add backend & design constants

--- lib/core/constants.dart
+++ lib/core/constants.dart
@@ -1,5 +1,10 @@
+import 'package:flutter/cupertino.dart';
+
 abstract class AppSettings {
+  //backend
   static const String appEndPoint = 'http://meyo.ddns.net:3333/v1';
-  static const String projectId = '62721b7fcb0281e68ff1';
+  static const String projectId = '627246dc8443dbd5dc68';
   static const bool appSelfSigned = true;
+  //design
+  static const designSize = Size(392.72727272727275, 759.2727272727273);
 }
